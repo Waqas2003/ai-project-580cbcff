d0f30d4675535ef9fcd2cffa3351fa9738165e63
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 23:35:14 +0500

    Add src\Checkout.js

---
+++ src\Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>This is the checkout page.</p>
+    </div>
+  );
+}
+
+export default Checkout;
+```
+
+This is a basic ecommerce website using React, with a navigation menu, home page, products page, product detail page, cart page, and checkout page. Note that this is just a starting point, and you will need to add more features and functionality to make it a fully functional ecommerce website.
